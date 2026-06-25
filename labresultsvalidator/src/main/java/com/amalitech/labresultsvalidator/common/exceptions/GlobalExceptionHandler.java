ca01ab2e3f179d8c6686e41c64bb501833fcbe08
Silas Kumi <silaskumi4@gmail.com>
2026-06-25 09:20:13 +0000

    refactor(exceptions): remove unused import from `GlobalExceptionHandler`

--- labresultsvalidator/src/main/java/com/amalitech/labresultsvalidator/common/exceptions/GlobalExceptionHandler.java
+++ labresultsvalidator/src/main/java/com/amalitech/labresultsvalidator/common/exceptions/GlobalExceptionHandler.java
@@ -22,7 +22,6 @@ import org.springframework.web.multipart.MaxUploadSizeExceededException;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
